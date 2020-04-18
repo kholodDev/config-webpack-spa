e5c5cded080da391b98c530fc5e619004deb1490
Чугунов Максим <m.chugunov@zebrains.team>
2020-04-18 13:40:43 +0300

    Added rules

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import style from './style.scss'
 
 const App = () => {
-    return (
-        <h1 className={style.h1}>App</h1>
-    )
+	return <h1 className={style.h1}>App</h1>
 }
 
 export default App
